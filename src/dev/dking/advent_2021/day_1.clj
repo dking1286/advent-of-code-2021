60328a68fcf901d78a91acf012f6280dd0eeb03a
Daniel King <daniel.oliver.king@gmail.com>
2021-12-21 10:47:06 -0800

    Finish Day 1 Part 2.

--- src/dev/dking/advent_2021/day_1.clj
+++ src/dev/dking/advent_2021/day_1.clj
@@ -21,9 +21,34 @@
        (filter (fn [[x y]] (> y x)))
        count))
 
+(defn part-2
+  []
+  (->> (str/split-lines (slurp (io/file (io/resource "day_01_part_01.txt"))))
+       (map #(Integer/parseInt %))
+       (partition 3 1)
+       (map (partial reduce +))
+       (partition 2 1)
+       (filter (fn [[x y]] (> y x)))
+       count))
+
 (comment
   (->> (str/split-lines (slurp (io/file (io/resource "day_01_part_01.txt"))))
        (map #(Integer/parseInt %))
+       (partition 2 1)
+       (filter (fn [[x y]] (> y x)))
+       count)
+
+  (->> part-1-example
+       (partition 3 1)
+       (map (partial reduce +))
+       (partition 2 1)
+       (filter (fn [[x y]] (> y x)))
+       count)
+
+  (->> (str/split-lines (slurp (io/file (io/resource "day_01_part_01.txt"))))
+       (map #(Integer/parseInt %))
+       (partition 3 1)
+       (map (partial reduce +))
        (partition 2 1)
        (filter (fn [[x y]] (> y x)))
        count))
