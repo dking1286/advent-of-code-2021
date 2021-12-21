dfa52dee6975dee837442883f848f70ff8e75038
Daniel King <daniel.oliver.king@gmail.com>
2021-12-21 10:41:56 -0800

    Finish Day 1 Part 1.

--- src/dev/dking/advent_2021/day_1.clj
+++ src/dev/dking/advent_2021/day_1.clj
@@ -1,1 +1,29 @@
-(ns dev.dking.advent-2021.day-1)
+(ns dev.dking.advent-2021.day-1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def part-1-example [199
+                     200
+                     208
+                     210
+                     100
+                     207
+                     240
+                     269
+                     260
+                     263])
+
+(defn part-1
+  []
+  (->> (str/split-lines (slurp (io/file (io/resource "day_01_part_01.txt"))))
+       (map #(Integer/parseInt %))
+       (partition 2 1)
+       (filter (fn [[x y]] (> y x)))
+       count))
+
+(comment
+  (->> (str/split-lines (slurp (io/file (io/resource "day_01_part_01.txt"))))
+       (map #(Integer/parseInt %))
+       (partition 2 1)
+       (filter (fn [[x y]] (> y x)))
+       count))
